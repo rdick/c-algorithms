2ecc49f3a3a1c7ab4a6aff292f2fc09b6a1f015d
rdick <62073529+rdick@users.noreply.github.com>
2022-04-29 01:34:14 -0700

    second largest number

--- arrays/largest_element.cpp
+++ arrays/largest_element.cpp
@@ -3,17 +3,22 @@ using namespace std;
 #include <string> 
 
 int largestElement (int arr[], int n) {
-    int max1 = 0;
+    int max1 = -1;
+    int max2 = -1;
 
     for(int i = 0; i < n; i++) {
-        max1 = max(max1, arr[i]);
+        if(arr[i] > max1){
+            max2 = max1;
+            max1 = arr[i];
+        } else if(arr[i] > max2 && arr[i] != max1){
+            max2 = arr[i];
+        }
     }
-    cout << max1 << endl;
-    return max1;
+    cout << max1 << " " << max2 << endl;
 }
 
 int main() {
-    int dog[] = {5,8,20,10};
+    int dog[] = {20,20,20,20,20};
     int val = largestElement(dog, 4);
     cout << val << endl;
 }
