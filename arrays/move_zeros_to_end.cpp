c6605fe530e681088fe4aa3ca95390abf7a43d34
rdick <62073529+rdick@users.noreply.github.com>
2022-04-29 02:04:37 -0700

    move zeros to end

---
+++ arrays/move_zeros_to_end.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+#include <string> 
+
+int largestElement (int arr[], int n) {
+    int first = 0 ;
+    int second = 1;
+    while(second < n) {
+        int first_num = arr[first];
+        int second_num = arr[second];
+        if(first_num == 0) {
+            if(second_num == 0){
+                second ++;
+            } else {
+                int temp = arr[first];
+                arr[first] = arr[second];
+                arr[second] = temp;
+                first++;
+                second++;
+            }
+        } else {
+            first++;
+            second++;
+        }
+    }
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << endl;
+    }
+}
+
+int main() {
+    int dog[] = {1,0,2,0,3,0};
+    int val = largestElement(dog, 6);
+}
+
